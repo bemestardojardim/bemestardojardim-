9431f26542090b42d57b6fb9bcdd006677cbcc92
Bem Estar Do Jardim <56318003+bemestardojardim@users.noreply.github.com>
2023-03-07 10:25:58 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -2,3 +2,15 @@
   <img src="ruta_de_la_imagen_instagram.png" alt="Instagram">
   Síguenos en Instagram
 </button>
+
+/* Capturamos el evento 'scroll' */
+window.addEventListener('scroll', () => {
+    /* Obtenemos la posición actual del desplazamiento */
+    const scrollTop = window.pageYOffset;
+
+    /* Movemos la capa de fondo del contenido a una velocidad más lenta */
+    document.querySelector('.content').style.transform = `translate3d(0, ${scrollTop * -0.3}px, 0)`;
+
+    /* Movemos la capa de fondo de la imagen */
+    document.querySelector('body:before').style.transform = `translate3d(0, ${scrollTop * 0.5}px, 0)`;
+});
