42a15ed816431889405ee28cf3881707fc6aaa4b
Bem Estar Do Jardim <56318003+bemestardojardim@users.noreply.github.com>
2023-03-10 12:20:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,14 +10,5 @@ $(window).scroll(function() {
 
 
 
-  const arrayHeight = document.getElementsByClassName('height-nav');
-            const navHeight = document.getElementsByTagName('nav')[0].clientHeight;
 
-            for (let navHeightObj of arrayHeight)
-                navHeightObj.style.height = navHeight + 'px';
 
-            function abrirMenu() {
-                const botoesMenu = document.getElementById('botoesMenu');
-
-                botoesMenu.className = botoesMenu.className.includes('responsivo') ? '' : 'responsivo'
-            }
