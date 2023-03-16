718fdd67d477abdd5123750c1eabc61c7b24e695
Bem Estar Do Jardim <56318003+bemestardojardim@users.noreply.github.com>
2023-03-16 13:31:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -45,3 +45,43 @@ document.getElementById("ver-rutas-btn").addEventListener("click", function() {
 
 
 
+
+
+
+
+function mostrarHorario() {
+			var hoy = new Date();
+			var dia = hoy.getDay();
+			var hora = hoy.getHours();
+			var minutos = hoy.getMinutes();
+			var abierto = false;
+			var texto = "Fechado";
+
+		
+    
+    
+    if (dia === 0 && hora < 7) {
+texto = "Abre hoje às 7:00";
+} else if (dia === 6) {
+texto = "Abre amanhã às 7:00";
+} else if (dia === 5 && hora >= 17 || dia === 6) {
+texto = "Abre no domingo às 7:00";
+} else if (hora >= 7 && (hora < 11 || (hora === 11 && minutos < 30))) {
+texto = "Aberto até às 11:30";
+abierto = true;
+} else if (hora >= 13 && hora < 17) {
+texto = "Aberto até às 17:30";
+abierto = true;
+} else if (dia !== 0 && dia !== 5 && dia !== 6) {
+texto = "Abre hoje às 13:00";
+}
+
+
+			document.getElementById("horario").innerHTML = texto;
+
+			if (abierto) {
+				setTimeout(mostrarHorario, 1000); // actualiza cada segundo si está abierto
+			} else {
+				setTimeout(mostrarHorario, 60000); // actualiza cada minuto si está cerrado
+			}
+		}
